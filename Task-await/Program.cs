c9a62be1ccd8a9e6f67b4ae1f7e57b7fe40a3033
Jean Michael <jeanmmclaudino@gmail.com>
2022-03-23 20:24:14 -0300

    Add project files.

---
+++ Task-await/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_await
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("Tecle algo para iniciar...\n");
+            Console.ReadKey();
+
+            await Aguardar(5);
+
+            Console.WriteLine("Já passou 5 segundos ...\n");
+            Console.WriteLine("fim");
+            Console.ReadLine();
+        }
+
+        static async Task Aguardar(int tempo)
+        {
+            Console.WriteLine("Iniciando espera...");
+            await Task.Delay(TimeSpan.FromSeconds(tempo));
+            Console.WriteLine("Fim da espera...");
+        }
+    }
+}
